53f96eadd43652477d7a760aa53a20dc9808f3a8
yongwoo-jeong <jywoo9@gmail.com>
2022-01-23 16:26:58 +0900

    changing categories by changing array index

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,25 +1,58 @@
+import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  const onClick = (newCategory: IToDo["category"]) => {
-    console.log(newCategory);
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>Doing</button>
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
       )}
       {category !== "TO_DO" && (
-        <button onClick={() => onClick("TO_DO")}>To Do</button>
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
       )}
       {category !== "DONE" && (
-        <button onClick={() => onClick("DONE")}>Done</button>
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
       )}
     </li>
   );
 }
 
 export default ToDo;
+
+/*
+setTodos((prevTodos) => {
+  const targetIndex = prevTodos.findIndex((todo) => todo.id === id);
+  const newTodo: ITodo = {
+    id,
+    text,
+    category: categoryState as ITodo["category"],
+  };
+  const newTodos = [...prevTodos]; // create new array
+  newTodos.splice(targetIndex, 1, newTodo);
+  return newTodos;
+});
+*/
