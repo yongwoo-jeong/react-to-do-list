3c67f3240d417585a6f7b31d2f1cd603757500b4
yongwoo-jeong <jywoo9@gmail.com>
2022-02-02 19:35:23 +0900

    react-beautiful-dnd structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ function App() {
         <Droppable droppableId="one">
           {() => (
             <ul>
-              <Draggable draggableId="">{() => <li>Hello</li>}</Draggable>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
             </ul>
           )}
         </Droppable>
