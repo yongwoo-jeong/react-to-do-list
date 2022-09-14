ff7c9c91b1552c8d21ac4ec36cb50c8efc56be40
yongwoojeong <jywoo9@gmail.com>
2022-09-14 21:17:14 +0900

    Feature - divide by category using Selector in Recoil

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,17 +7,33 @@ import ToDo from "./ToDo";
 //
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo {...toDo} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo {...toDo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
